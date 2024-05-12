cdddfa594744b4d0b0c9e293859c79238fe26cbe
ttravv <161491928+ttravv@users.noreply.github.com>
2024-05-12 16:31:40 +0300

    two pointers

---
+++ two pointers.cpp
@@ -0,0 +1,39 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> func(vector<int>& arr) {
+    vector<int> new_arr;
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int powr = pow(arr[right], 2);
+        int powl = pow(arr[left], 2);
+        if (powr > powl) {
+            right--;
+            new_arr.push_back(powr);
+        }
+        else {
+            left++;
+            new_arr.push_back(powl);
+        }
+    }
+    return new_arr;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    auto ans = func(arr);
+    for (auto x : ans) {
+        cout << x << ' ';
+    }
+    return 0;
+}
