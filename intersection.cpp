13bdc5593c07f40159347215e7a06267a67b3811
ttravv <161491928+ttravv@users.noreply.github.com>
2024-05-28 17:41:15 +0300

    leetcode intersection

---
+++ intersection.cpp
@@ -0,0 +1,32 @@
+﻿//https://leetcode.com/problems/intersection-of-two-arrays/description/
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+	vector<int> ans;
+	vector<pair<int, int>> mapForNumsOne;
+	vector<pair<int, int>> mapForNumsTwo;
+	for (int i = 0; i < nums1.size(); ++i) {
+		
+	}
+
+	return ans;
+}
+
+
+
+int main() {
+	vector<int> nums1 = { 1, 2, 2, 1 };
+	vector<int> nums2 = { 2,2 };
+	vector<int> ans = intersection(nums1, nums2);
+	for (int i = 0; i < ans.size(); ++i) {
+		cout << ans[i];
+	}
+	
+
+	return 0;
+}
