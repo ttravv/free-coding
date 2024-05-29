3fe67efc0e2b998590bd6a2da2e044ba98b22220
ttravv <161491928+ttravv@users.noreply.github.com>
2024-05-29 16:53:11 +0300

    reversed words of string

--- something.cpp
+++ something.cpp
@@ -1,67 +1,35 @@
-﻿#include <iostream>
+﻿//https://leetcode.com/problems/reverse-words-in-a-string-iii/description/?envType=problem-list-v2&envId=rbtkeej1
+
+#include <iostream>
+#include <algorithm>
 #include <vector>
-#include <unordered_map>
-#include <set>
 using namespace std;
 
-void task(vector<int>& arr, vector<int>& arr2, vector<int>& arr3) {
-    unordered_map<int, int> map;
-    set<int> unique;
-    set<int> unique2;
-    set<int> unique3;
-    for (int i = 0; i < arr.size(); ++i) {
-        unique.insert(arr[i]);
-    }
-    for (int j = 0; j < arr2.size(); ++j) {
-        unique2.insert(arr2[j]);
-
-    }
-    for (int h = 0; h < arr3.size(); ++h) {
-        unique3.insert(arr3[h]);
-    }
-    for (int i : unique) {
-        map[i] += 1;
-    }
-    for (int i : unique2) {
-        map[i] += 1;
-    }
-    for (int i : unique3) {
-        map[i] += 1;
-    }
-    for (auto v : map) {
-        if (v.second >= 2) {
-            cout << v.first;
+string reverseWords(string s) {
+    vector<string> new_s;
+    string word = "";
+    for (int i = 0; i < s.length(); ++i) {
+        if (s[i] == ' ') {
+            new_s.push_back(word);
+            word = "";
+            continue;
         }
+        word += s[i];
     }
+    new_s.push_back(word);
+    string ans = "";
+    for (int i = 0; i < new_s.size(); ++i) {
+        reverse(new_s[i].begin(), new_s[i].end());
+        if (i != new_s.size() - 1)ans += new_s[i] + ' ';
+        else ans += new_s[i];
+    }
+    return ans;
+
 }
 
-
 int main() {
-    int n;
+    string s = "Let's take LeetCode contest";
 
-    cin >> n;
-
-    vector<int> arr;
-    for (int i = 0; i < n; ++i) {
-        int temp;
-        cin >> temp;
-        arr.push_back(temp);
-    }
-    cin >> n;
-
-    vector<int> arr2;
-    for (int h = 0; h < n; ++h) {
-        int temp;
-        cin >> temp;
-        arr2.push_back(temp);
-    }
-    cin >> n;
-    vector<int> arr3;
-    for (int j = 0; j < n; ++j) {
-        int temp;
-        cin >> temp;
-        arr3.push_back(temp);
-    }
-    task(arr, arr2, arr3);
+    cout << reverseWords(s);
     return 0;
 }
