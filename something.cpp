269c81a3ae24f0a7531870ab8664a9056ec9af1d
ttravv <161491928+ttravv@users.noreply.github.com>
2024-06-05 12:00:33 +0300

    leetcode task solution

--- something.cpp
+++ something.cpp
@@ -1,35 +1,56 @@
-﻿//https://leetcode.com/problems/reverse-words-in-a-string-iii/description/?envType=problem-list-v2&envId=rbtkeej1
+﻿//leetcode task
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-string reverseWords(string s) {
-    vector<string> new_s;
-    string word = "";
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] == ' ') {
-            new_s.push_back(word);
-            word = "";
-            continue;
+int search(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int low_ind = 0;
+    int high_ind = nums.size() - 1;
+    while (low_ind <= high_ind) {
+        int mid = (low_ind + high_ind) / 2;
+        if (nums[mid] == target) {
+            return mid;
         }
-        word += s[i];
+        if (nums[mid] > target) {
+            high_ind = mid - 1;
+        }
+        else {
+            low_ind = mid + 1;
+        }
     }
-    new_s.push_back(word);
-    string ans = "";
-    for (int i = 0; i < new_s.size(); ++i) {
-        reverse(new_s[i].begin(), new_s[i].end());
-        if (i != new_s.size() - 1)ans += new_s[i] + ' ';
-        else ans += new_s[i];
-    }
-    return ans;
-
+    return -1;
 }
 
-int main() {
-    string s = "Let's take LeetCode contest";
+vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+    vector<vector<int>>  ans(2);
+    for (int i = 0; i < nums1.size(); ++i) {
+        if ((search(ans[0], nums1[i]) == -1) && (search(nums2, nums1[i]) == -1)) {
+            ans[0].push_back(nums1[i]);
+        }
+    }
+    for (int i = 0; i < nums2.size(); ++i) {
+        if (search(ans[1], nums2[i]) == -1 && search(nums1, nums2[i]) == -1) {
+            ans[1].push_back(nums2[i]);
+        }
+    }
+    return ans;
+}
 
-    cout << reverseWords(s);
+
+int main() {
+    vector<int> nums1 = { 26,48,-78,-25,42,-8,94,-68,26 };
+    vector<int> nums2 = { 61,-17 }; //[[26,48,-78,-25,42,-8,94,-68,26],[61,-17]]
+    auto ans = findDifference(nums1, nums2);
+    for (int i = 0; i < ans.size(); ++i) {
+        for (int j = 0; j < ans[i].size(); ++j) {
+            cout << ans[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+
     return 0;
 }
