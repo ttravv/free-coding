3bbef68d12cb634a7ff417a3148b6912c281e547
ttravv <161491928+ttravv@users.noreply.github.com>
2024-06-12 15:56:01 +0300

    new

--- firstuniqchar.cpp
+++ firstuniqchar.cpp
@@ -1,17 +1,35 @@
-﻿#include <iostream>
+﻿// https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+#include <iostream>
 #include <string>
-#include <algorithm>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
-auto highAndLow(const string& numbers) {
-	auto max = max_element(numbers.begin(), numbers.end());
-	auto min = min_element(numbers.begin(), numbers.end());
-	return max
+int firstUniqChar(string s) {
+    vector<int> array(26);
+    for (int i = 0; i < s.length(); ++i) {
+        if (array[s[i] - 'a'] != 0) {
+            array[s[i] - 'a'] = -1;
+        }
+        else array[s[i] - 'a'] = i + 1;
+    }
+    int ans = s.length() + 1;
 
+    for (int i = 0; i < 26; ++i) {
+        if ((array[i] < ans) && array[i] > 0)
+            ans = array[i];
+    }
+
+    if (ans == (s.length() + 1)) {
+        return -1;
+    }
+    return ans - 1;
 }
-int main() {
-	string numbers = "8 3 -5 42 -1 0 0 -9 4 7 4 -4"; // '42, -9'
-	cout << highAndLow(numbers);
 
-	return 0;
+int main() {
+    string s = "aadadaad"; // -1
+    cout << firstUniqChar(s);
+
+    return 0;
 }
